9e9ad21aa507524080a1d9c7b8b7a0b9c325aae8
josymarss <sylvajosymar@gmail.com>
2024-10-24 02:15:29 +0100

    Gitignore and setup windowscene with navigation

---
+++ uikitapp/PeopleViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PeopleViewController.swift
+//  uikitapp
+//
+//  Created by Josymarss on 24/10/24.
+//
+
+import UIKit
+
+class PeopleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUp()
+    }
+
+
+}
+
+
+private extension PeopleViewController {
+    func setUp(){
+        self.view.backgroundColor = .white
+    }
+}
+
