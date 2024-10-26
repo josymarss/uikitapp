f898cb1e9b6ce1d4a652c343fc109bd435809e50
josymarss <sylvajosymar@gmail.com>
2024-10-26 03:11:42 +0100

    Use UICollectionView to listing fecth

--- uikitapp/PeopleViewController.swift
+++ uikitapp/PeopleViewController.swift
@@ -7,60 +7,20 @@
 
 import UIKit
 
-class PeopleViewController: UIViewController {
+class PeopleViewController: UIViewController{
     
-    private lazy var subscribeButton: UIButton = {
-        
-        var configButton = UIButton.Configuration.filled()
-        configButton.title = "Subscribe".uppercased()
-        configButton.baseBackgroundColor = .red
-        configButton.baseForegroundColor = .white
-        configButton.buttonSize = .large
-        configButton.cornerStyle = .medium
-        
-        let button = UIButton(configuration: configButton)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        
-        // MARK: add event
-        button.addTarget(self, action: #selector(didTapSubscribe), for: .touchUpInside)
-        return button
-    }()
+    private var peopleViewModel = PeopleViewModel()
     
-    
-    private lazy var personContainerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor.gray.withAlphaComponent(0.1)
-        view.translatesAutoresizingMaskIntoConstraints = false
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = .init(width:UIScreen.main.bounds.width, height:139)
         
-        return view
-    }()
-    
-    private lazy var nameLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Josemar Silva"
-        label.font = .systemFont(ofSize: 24, weight: .bold)
-        label.translatesAutoresizingMaskIntoConstraints = false
+        let collView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collView.translatesAutoresizingMaskIntoConstraints = false
+        collView.register(PersonCollectionViewCell.self, forCellWithReuseIdentifier: "PersonCollectionViewCell")
+        collView.dataSource = self
         
-        return label
-    }()
-    
-    private lazy var emailLabel: UILabel = {
-        let label = UILabel()
-        label.text = "josymarss@mail.com"
-        label.font = .systemFont(ofSize: 15, weight: .regular)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        return label
-    }()
-    
-    private lazy var personStackView: UIStackView = {
-        let stackView = UIStackView()
-        
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.spacing = 8
-        
-        return stackView
+        return collView
     }()
     
     override func viewDidLoad() {
@@ -68,9 +28,11 @@ class PeopleViewController: UIViewController {
         
         // MARK: SetUp and constraint
         setUp()
+        peopleViewModel.delegate = self
+        peopleViewModel.getUsers()
     }
-
-
+    
+    
 }
 
 
@@ -78,33 +40,45 @@ private extension PeopleViewController {
     
     func setUp(){
         self.view.backgroundColor = .white
+        self.view.addSubview(collectionView)
         
-        // MARK: Add into a view
-        self.view.addSubview(personContainerView)
-        personContainerView.addSubview(personStackView)
-        
-        // MARK: Add to Stack
-        personStackView.addArrangedSubview(nameLabel)
-        personStackView.addArrangedSubview(emailLabel)
-        personStackView.addArrangedSubview(subscribeButton)
-        
-        // MARK: Position in the screen
         NSLayoutConstraint.activate([
-            personContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            personContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
-            personContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
             
-            personStackView.topAnchor.constraint(equalTo: personContainerView.topAnchor, constant: 8),
-            personStackView.leadingAnchor.constraint(equalTo: personContainerView.leadingAnchor, constant: 8),
-            personStackView.trailingAnchor.constraint(equalTo: personContainerView.trailingAnchor, constant: -8),
-            personStackView.bottomAnchor.constraint(equalTo: personContainerView.bottomAnchor, constant: -8)
-
         ])
     }
     
-    // MARK: Events
-    @objc func didTapSubscribe(sender: UIButton) {
-        print("You're subscribed now!")
+}
+
+extension PeopleViewController:PeopleViewModelDelegate {
+    
+    func didFinish() {
+        collectionView.reloadData()
+    }
+    
+    func didFail(error: Error) {
+        print(error)
     }
 }
 
+extension PeopleViewController: UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        peopleViewModel.people.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let item = peopleViewModel.people[indexPath.item]
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PersonCollectionViewCell", for: indexPath) as! PersonCollectionViewCell
+        
+        cell.item = item
+        
+        return cell
+    }
+    
+}
---
+++ uikitapp/PersonCollectionViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  PersonCollectionViewCell.swift
+//  uikitapp
+//
+//  Created by Josymarss on 26/10/24.
+//
+
+import UIKit
+
+class PersonCollectionViewCell: UICollectionViewCell {
+    
+    private var personView: PersonView?
+    
+    var item: PersonResponse? {
+        didSet{
+            guard let firstName = item?.firstName, let lastName = item?.lastName, let letEmail = item?.email else { return }
+            
+            personView?.setNameAndEmail(name: "\(firstName)", lastName: "\(lastName)", email: "\(letEmail)")
+        }
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension PersonCollectionViewCell {
+    
+    func setUp(){
+        guard personView == nil else { return }
+        
+        personView = PersonView ()
+        
+        self.contentView.addSubview(personView!)
+        
+        NSLayoutConstraint.activate([
+            personView!.topAnchor.constraint(equalTo: contentView.topAnchor),
+            personView!.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            personView!.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            personView!.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+}
